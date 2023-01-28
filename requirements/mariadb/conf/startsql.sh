79259ce33d6229826f74ad1f05b1543ff17fe365
Ayblin42 <propaxh20@gmail.com>
2023-01-28 19:27:37 +0100

    mariadb error

--- requirements/mariadb/conf/startsql.sh
+++ requirements/mariadb/conf/startsql.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
+
+# Start the MySQL service
 service mysql start;
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
+
+mysql -u root -p${SQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
+if mysql -u root -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user='root' and host='localhost')" | grep -q 1; then
+
+    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+    mysql -u root -p${SQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+    mysql -u root -p${SQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%';"
+    mysql -u root -p${SQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+fi
+
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 exec mysqld_safe
